92b955035c4247450b3432e2d619c4e96f223248
senpainikolay <nicolae32gherman@gmail.com>
2023-03-07 09:45:17 +0200

    add tema 4

---
+++ Tema 04 - Testing/PetShelter/PetShelter.BusinessLayer.Tests/AddDonationTests.cs
@@ -0,0 +1,140 @@
+﻿using FluentAssertions;
+using Moq;
+using PetShelter.BusinessLayer.ExternalServices;
+using PetShelter.BusinessLayer.Validators;
+using PetShelter.DataAccessLayer.Models; 
+
+using PetShelter.DataAccessLayer.Repository;
+using PetShelter.BusinessLayer.Models;
+
+
+
+namespace PetShelter.BusinessLayer.Tests;
+
+public class AddDonationTests
+{
+
+
+    private readonly IPersonService _personService;
+    private readonly DonationService _donationServiceSut;
+
+    private readonly Mock<IPersonRepository> _mockPersonRepository;
+    private readonly Mock<IDonationRepository> _mockDonationRepository;
+    private readonly Mock<IIdNumberValidator> _mockIdNumberValidator;
+
+
+    public readonly static IEnumerable<object[]> InvalidPetPersonDatesOfBirth = new List<object[]>{
+
+              new object[] { DateTime.Now},
+              new object[] { DateTime.Now.AddYears(-5) },
+               new object[] {DateTime.Now.AddYears(-17).AddMonths(-11).AddDays(-27) }
+        };
+
+    private AddDonationRequest _request;
+
+    public AddDonationTests()
+    {
+        _mockPersonRepository = new Mock<IPersonRepository>();
+        _mockDonationRepository = new Mock<IDonationRepository>();
+        _mockIdNumberValidator = new Mock<IIdNumberValidator>();
+
+        _personService = new PersonService(_mockPersonRepository.Object, _mockIdNumberValidator.Object, new PersonValidator());
+        _donationServiceSut = new DonationService(_mockDonationRepository.Object, new AddDonationRequestValidator());
+    }
+
+    private void SetupHappyPath()
+    {
+        _mockIdNumberValidator.Setup(x => x.Validate(It.IsAny<string>())).ReturnsAsync(true);
+
+        _request = new AddDonationRequest
+        {
+            Amount = 10,
+            Donor = new DataAccessLayer.Models.Person
+            {
+                DateOfBirth = DateTime.Now.AddYears(-Constants.PersonConstants.AdultMinAge - 2),
+                IdNumber = "1111222233333",
+                Name = "OKlolA"
+            }
+
+        };
+    }
+
+    [Fact]
+    public async Task GivenValidRequest_WhenAddDonation_DonationIsAdded()
+    {
+        SetupHappyPath();
+
+        await _donationServiceSut.AddDonation(_request);
+
+        _mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(d => d.Amount == _request.Amount)), Times.Once);
+    }
+
+
+
+
+    [Theory]
+    [InlineData(-5)]
+    [InlineData(0)]
+    public async Task GiventAmountIsInvalid_WhenAddDonation_ThenThrowsArgumentException_And_DonationIsNotAdded(int donationAmount)
+    {
+        // Arrange
+        SetupHappyPath();
+        _request.Amount = donationAmount;
+
+        //Act
+        await Assert.ThrowsAsync<ArgumentException>(() => _donationServiceSut.AddDonation(_request));
+
+        //Assert
+        _mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(p => p.Amount == _request.Amount)), Times.Never);
+    }
+
+    [Fact]
+    public async Task GivenIdNumberIsInvalid_WhenAddDonation_ThenThowsArgumentException_And_DonationIsNotAdded()
+    {
+        //Arrange
+        SetupHappyPath();
+        _mockIdNumberValidator.Setup(x => x.Validate(It.IsAny<string>())).ReturnsAsync(false);
+        _request.Donor.IdNumber = "821214441242";
+
+        //Act 
+         await Assert.ThrowsAsync<ArgumentException>(() => _donationServiceSut.AddDonation(_request));
+
+
+
+        //Assert
+        _mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(p => p.Amount == _request.Amount)), Times.Never);
+    }
+
+
+    [Fact]
+    public async Task GivenDonorNameIsInvalid_WhenAddDonation_ThenThrowsArgumentException_And_DonationIsNotAdded()
+    {
+        // Arrange
+        SetupHappyPath();
+        _request.Donor.Name = "P";
+
+        //Act
+        await Assert.ThrowsAsync<ArgumentException>(() => _donationServiceSut.AddDonation(_request));
+
+
+        //Assert
+        _mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(p => p.Amount == _request.Amount)), Times.Never);
+    }
+
+
+
+    [Theory]
+    [MemberData(nameof(InvalidPetPersonDatesOfBirth))]
+    public async Task GiventDonorDateOfBirthIsInvalid_WhenAddDonation_ThenThrowsArgumentException_And_DonationIsNotAdded(DateTime dateOfBirth)
+    {
+        // Arrange
+        SetupHappyPath();
+        _request.Donor.DateOfBirth = dateOfBirth;
+
+        //Act
+        await Assert.ThrowsAsync<ArgumentException>(() => _donationServiceSut.AddDonation(_request));
+
+        //Assert
+        _mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(p => p.Amount == _request.Amount)), Times.Never);
+    }
+}
---
+++ Tema 04 - Testing/PetShelter/PetShelter.DataAccessLayer/Migrations/20230217092343_RenameColumnsById.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShelter.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnsById : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Donations_Persons_DonatedById",
+                table: "Donations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Persons_AdoptedById",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Persons_RescuedById",
+                table: "Pets");
+
+            migrationBuilder.RenameColumn(
+                name: "RescuedById",
+                table: "Pets",
+                newName: "RescuerId");
+
+            migrationBuilder.RenameColumn(
+                name: "AdoptedById",
+                table: "Pets",
+                newName: "AdopterId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pets_RescuedById",
+                table: "Pets",
+                newName: "IX_Pets_RescuerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pets_AdoptedById",
+                table: "Pets",
+                newName: "IX_Pets_AdopterId");
+
+            migrationBuilder.RenameColumn(
+                name: "DonatedById",
+                table: "Donations",
+                newName: "DonorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Donations_DonatedById",
+                table: "Donations",
+                newName: "IX_Donations_DonorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donations_Persons_DonorId",
+                table: "Donations",
+                column: "DonorId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Persons_AdopterId",
+                table: "Pets",
+                column: "AdopterId",
+                principalTable: "Persons",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Persons_RescuerId",
+                table: "Pets",
+                column: "RescuerId",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Donations_Persons_DonorId",
+                table: "Donations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Persons_AdopterId",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Persons_RescuerId",
+                table: "Pets");
+
+            migrationBuilder.RenameColumn(
+                name: "RescuerId",
+                table: "Pets",
+                newName: "RescuedById");
+
+            migrationBuilder.RenameColumn(
+                name: "AdopterId",
+                table: "Pets",
+                newName: "AdoptedById");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pets_RescuerId",
+                table: "Pets",
+                newName: "IX_Pets_RescuedById");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pets_AdopterId",
+                table: "Pets",
+                newName: "IX_Pets_AdoptedById");
+
+            migrationBuilder.RenameColumn(
+                name: "DonorId",
+                table: "Donations",
+                newName: "DonatedById");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Donations_DonorId",
+                table: "Donations",
+                newName: "IX_Donations_DonatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donations_Persons_DonatedById",
+                table: "Donations",
+                column: "DonatedById",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Persons_AdoptedById",
+                table: "Pets",
+                column: "AdoptedById",
+                principalTable: "Persons",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Persons_RescuedById",
+                table: "Pets",
+                column: "RescuedById",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+    }
+}
